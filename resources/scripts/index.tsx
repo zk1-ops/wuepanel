50cbee413c4ff2db9af69010b4509dfde5b18177
NoHaxito <santifeb2034@gmail.com>
2022-03-23 22:48:57 +0000

    Oli

---
+++ resources/scripts/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '@/components/App';
+import './i18n';
+import { setConfig } from 'react-hot-loader';
+import '@/assets/css/main.css';
+import '@/assets/css/bootstrap.min.css';
+import '@/assets/css/materialdesignicons.min.css';
+import '@/assets/css/lineicons.css';
+// Prevents page reloads while making component changes which
+// also avoids triggering constant loading indicators all over
+// the place in development.
+//
+// @see https://github.com/gaearon/react-hot-loader#hook-support
+setConfig({ reloadHooks: false });
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ resources/scripts/routers/DashboardRouter.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { NavLink, Route, RouteComponentProps, Switch } from 'react-router-dom';
+import AccountOverviewContainer from '@/components/dashboard/AccountOverviewContainer';
+import NavigationBar from '@/components/NavigationBar';
+import DashboardContainer from '@/components/dashboard/DashboardContainer';
+import AccountApiContainer from '@/components/dashboard/AccountApiContainer';
+import { NotFound } from '@/components/elements/ScreenBlock';
+import TransitionRouter from '@/TransitionRouter';
+import SubNavigation from '@/components/elements/SubNavigation';
+
+export default ({ location }: RouteComponentProps) => (
+    <>
+    <aside className="sidebar-nav-wrapper">
+                <div className="navbar-logo">
+                    <a href="index.html">
+                        <img src="assets/images/logo/logo.svg" alt="logo"></img>
+                    </a>
+                </div>
+                <nav className="sidebar-nav">
+                    <ul>
+                        <li className="nav-item">
+                            <a href="notification.html">
+                                <span className="icon">
+                                    <svg
+                                        width="22"
+                                        height="22"
+                                        viewBox="0 0 22 22"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M9.16667 19.25H12.8333C12.8333 20.2584 12.0083 21.0834 11 21.0834C9.99167 21.0834 9.16667 20.2584 9.16667 19.25ZM19.25 17.4167V18.3334H2.75V17.4167L4.58333 15.5834V10.0834C4.58333 7.24171 6.41667 4.76671 9.16667 3.94171V3.66671C9.16667 2.65837 9.99167 1.83337 11 1.83337C12.0083 1.83337 12.8333 2.65837 12.8333 3.66671V3.94171C15.5833 4.76671 17.4167 7.24171 17.4167 10.0834V15.5834L19.25 17.4167ZM15.5833 10.0834C15.5833 7.51671 13.5667 5.50004 11 5.50004C8.43333 5.50004 6.41667 7.51671 6.41667 10.0834V16.5H15.5833V10.0834Z"
+                                        />
+                                    </svg>
+                                </span>
+                                <span className="text">Dashboard</span>
+                            </a>
+                        </li>
+<li className="nav-item nav-item-has-children">
+            <a
+              href="#0"
+              data-bs-toggle="collapse"
+              data-bs-target="#ddmenu_1"
+              aria-controls="ddmenu_1"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="icon">
+                <svg width="22" height="22" viewBox="0 0 22 22">
+                  <path
+                    d="M17.4167 4.58333V6.41667H13.75V4.58333H17.4167ZM8.25 4.58333V10.0833H4.58333V4.58333H8.25ZM17.4167 11.9167V17.4167H13.75V11.9167H17.4167ZM8.25 15.5833V17.4167H4.58333V15.5833H8.25ZM19.25 2.75H11.9167V8.25H19.25V2.75ZM10.0833 2.75H2.75V11.9167H10.0833V2.75ZM19.25 10.0833H11.9167V19.25H19.25V10.0833ZM10.0833 13.75H2.75V19.25H10.0833V13.75Z"
+                  />
+                </svg>
+              </span>
+              <span className="text">Dashboard</span>
+            </a>
+            <ul id="ddmenu_1" className="collapse show dropdown-nav">
+              <li>
+                <a 
+                href="/account" 
+                className="active"
+                >
+                  <span className="text">
+                    Overview
+                  </span>
+                </a>
+                
+              </li>
+              <li>
+                <a 
+                href="/account/api"
+                className="active"
+                >
+                
+                  <span className="text">
+                    Api Keys
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+                    </ul>
+                </nav>
+            </aside>
+            <div className="overlay"></div>
+       <main className="main-wrapper">
+        <NavigationBar/>
+        <TransitionRouter>
+            <Switch location={location}>
+                <Route path={'/'} exact>
+                    <DashboardContainer/>
+                </Route>
+                <Route path={'/account'} exact>
+                    <AccountOverviewContainer/>
+                </Route>
+                <Route path={'/account/api'} exact>
+                    <AccountApiContainer/>
+                </Route>
+                <Route path={'*'}>
+                    <NotFound/>
+                </Route>
+            </Switch>
+        </TransitionRouter>
+        </main>
+    </>
+);
